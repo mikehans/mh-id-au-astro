38713a889611d693e0d454e93ee79e1925706614
Mike Hansford <mjhansford@gmail.com>
2024-04-24 21:25:53 +1000

    Add frontmatter schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,33 +1,18 @@
 import { z, defineCollection } from "astro:content";
 
-// const postsCollection = defineCollection({
-//     type: 'content',
-//     schema: z.object({
-//         title: z.string(),
-//         pubDate: z.date(),
-//         description: z.string(),
-//         imageUrl: z.string().url(),
-//         author: z.string(),
-//         type: z.string(),
-//         slug: z.string().optional(),
-//         tags: z.array(z.string())
-//     })
-// });
-
 const postsCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date({
-      required_error: "Please select a date and time",
-      invalid_type_error: "That's not a date!",
-    }),
-    description: z.string(),
-    imageUrl: z.string().url().optional(),
-    tags: z.array(z.string()).optional(),
-  }),
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        date: z.string(),
+        description: z.string().optional(),
+        imageUrl: z.string().optional(),
+        author: z.string(),
+        type: z.string(),
+        tags: z.array(z.string()).optional()
+    })
 });
 
 export const collections = {
-  posts: postsCollection,
+  articles: postsCollection,
 };
